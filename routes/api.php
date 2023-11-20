0fa0c2e946de790f43d1ec35a54e25509ed97900
JuanEstebanGonzalez19 <juanestebangonzalezsierra0327@gmail.com>
2023-11-20 00:19:15 -0500

    Ubicacion de Ruta para las carreras

--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\CarreraController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+//ruta carrera//
 Route::apiResource('carrera', CarreraController::class);
