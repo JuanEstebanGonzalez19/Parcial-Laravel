e250dd4e43f5cb51cedd73bc76b1b2ce6144be7d
JuanEstebanGonzalez19 <juanestebangonzalezsierra0327@gmail.com>
2023-11-20 00:20:59 -0500

    Ubicacion rutas generales por Web

--- routes/web.php
+++ routes/web.php
@@ -6,26 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\ProfileController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/',[PublicController::class,'index'])->name('public.index');
-
-// Route::get('/index', function () {
-//     return view('index');
-// })->middleware(['auth', 'verified'])->name('index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
